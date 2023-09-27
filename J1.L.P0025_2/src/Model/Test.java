e676208115bd4191de8beb8c39a86cec8662010f
Admin <hiennguyenhuu83@gmail.com>
2023-09-27 21:27:10 +0700

    .

--- J1.L.P0025_2/src/Model/Test.java
+++ J1.L.P0025_2/src/Model/Test.java
@@ -1,41 +1,140 @@
-    
+package Model;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.PrintWriter;
+
 
 public class Test {
+        //one space by special characters
+    public static String formatOneSpaceSpecial(String line, String character) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String[] strings = line.split("\\s*\\" + character + "\\s*");
+        //appen every word and character special distance is one space
+        for (String oneWord : strings) {
+            stringBuffer.append(oneWord + " " + character);
+            stringBuffer.append(" ");
+        }
+        return stringBuffer.toString().trim().substring(0, stringBuffer.length() - 3);
+    }
 
-    public static void main(String[] args) {
-        try {
-            StringBuffer sb = new StringBuffer();//instantiate a string buffer 
-            BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-            //instantiate a bufferresder to read from file
-            String text;
-            while ((text = br.readLine()) != null) {
-                //read the text into String buffer
-                sb.append(text + System.lineSeparator());
+    //only one space between words and all character lowercase
+    public static String formatOneSpace(String line) {
+        line = line.toLowerCase();
+        line = line.replaceAll("\\s+", " ");
+        line = formatOneSpaceSpecial(line, ".");
+        line = formatOneSpaceSpecial(line, ",");
+        line = formatOneSpaceSpecial(line, ":");
+        line = formatOneSpaceSpecial(line, "\"");
+        return line.trim();
+    }
+
+    //only one space after comma (,), dot (.) and colon (:). 
+    public static String formatSpecialCharacters(String line) {
+        StringBuffer stringBuffer = new StringBuffer(line);
+        //check from first to last before .,:; then delete
+        for (int i = 0; i < stringBuffer.length() - 1; i++) {
+            if (stringBuffer.charAt(i) == ' '
+                    && stringBuffer.charAt(i + 1) == '.'
+                    || stringBuffer.charAt(i + 1) == ','
+                    || stringBuffer.charAt(i + 1) == ':') {
+                stringBuffer.deleteCharAt(i);
             }
-            text = sb.toString(); //output to string variable 
-            System.out.println("Raw Text: \n" + text);
-            text = text.toLowerCase(); //change all characters to lowercase   
-            text = text.substring(0, 1).toUpperCase() + text.substring(1); //change first character to uppercase  
-            text = text.replaceAll("\\s{2,}", " "); //remove two or more spaces 
-            text = text.replaceAll("([\\s]+)([.,])", "$2"); //remove space before , or .    
-            text = text.replaceAll("([.,:])([\\S])", "$1 $2"); //add space after , or . or :  
-            text = text.replaceAll("(\")[\\s]*([\\w\\s]*[\\w])[\\s]*(\")", "$1$2$3"); //remove space after or before "  
-            text = text.replaceAll("([^.])$", "$1."); //add . at the end
-            Matcher m = Pattern.compile("([.])([\\s]*)([\\w])").matcher(text); //create a pattern for detecting . and a character
-            text = m.replaceAll(r -> r.group(1) + " " + r.group(3).toUpperCase()); //turn first character to uppercase 
-            text = text.replaceAll("([.])([\\s])([\\w])", "$1 $3");
-            text = text.substring(0, text.length() - 1);  //remove extra . at the end
-            System.out.println("Text after normalization: \n" + text);
-        } catch (FileNotFoundException e) { //if input file not found
-            System.out.println(e);
-        } catch (IOException e) {   //if no input is given to buffered reader
-            System.out.println(e);
+        }
+        return stringBuffer.toString().trim();
+    }
+
+    //first character of word after dot is in Uppercase and other words are in lower case.
+    public static String afterDotUpperCase(String line) {
+        StringBuffer stringBuffer = new StringBuffer(line);
+        int lengthLine = stringBuffer.length();
+        //check from first to last after . then UpperCase
+        for (int i = 0; i < lengthLine - 2; i++) {
+            if (stringBuffer.charAt(i) == '.') {
+                char afterDot = stringBuffer.charAt(i + 2);
+                stringBuffer.setCharAt(i + 2, Character.toUpperCase(afterDot));
+            }
+        }
+        return stringBuffer.toString().trim();
+    }
+
+    //there are no spaces before and after sentence or word phrases in quotes (“”).
+    static int countQuetes = 0;
+
+    public static String noSpaceQuotes(String line) {
+        StringBuffer stringBuffer = new StringBuffer(line);
+        for (int i = 0; i < stringBuffer.length(); i++) {
+            if (stringBuffer.charAt(i) == '"' && countQuetes % 2 == 0) {
+                stringBuffer.deleteCharAt(i + 1);
+                countQuetes++;
+            } else if (stringBuffer.charAt(i) == '"' && countQuetes % 2 == 1
+                    && i != 0) {
+                stringBuffer.deleteCharAt(i - 1);
+                countQuetes++;
+            }
+        }
+        return stringBuffer.toString().trim();
+    }
+
+    //first character of word in first line is in Uppercase
+    public static String firstUpercase(String line) {
+        line = line.substring(3);
+        StringBuffer stringBuffer = new StringBuffer(line);
+        for (int i = 0; i < line.length(); i++) {
+            if (Character.isLetter(line.charAt(i))) {
+                stringBuffer.setCharAt(i, Character.toUpperCase(line.charAt(i)));
+                break;
+            }
+        }
+        return stringBuffer.toString().trim();
+    }
+
+    //must have dot at the end of text.
+    public static String lastAddDot(String line) {
+        if (line.endsWith(".")) {
+            return line;
+        } else {
+            return line + ".";
         }
     }
+
+    //there are no blank line between lines
+    public static boolean isLineEmpty(String line) {
+        if (line.length() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //count line
+    public static int countLine() {
+        int countLine = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File("input.txt")));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt", true)));
+            String line;
+            //write until end file
+            while ((line = br.readLine()) != null) {
+                //check line empty
+                if (isLineEmpty(line)) {
+                    continue;
+                }
+                countLine++;
+            }
+            br.close();
+            pw.close();
+
+        } catch (FileNotFoundException ex) {
+            System.err.println("Can't found.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return countLine;
+    }
+
 }
